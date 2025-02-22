7332b122ed2989e8dd9182d1f255cb2bd5ce8fae
Jayanth <jay002.cse@gmail.com>
2025-02-22 16:16:08 +0530

    level 1 backend

--- User.js
+++ User.js
@@ -35,6 +35,10 @@ const userSchema=new mongoose.Schema(
             type:Boolean,
             default:false
         },
+        gridNumber:{
+            type: Number,
+            required:true
+        }
     
     }
 )
